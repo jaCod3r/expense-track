b50637cb95240d5315e3aabc9c50cc7f835853fd
Juber Ahmed <a.juber@hotmail.com>
2019-07-05 18:21:18 -0400

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,42 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-  entry: './public/src/app.jsx',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  const CSSExtract = new ExtractTextPlugin('styles.css')
+  console.log('env', env)
+  return {
+    entry: './public/src/app.jsx',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
 
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.jsx$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.jsx$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              { loader: 'css-loader', options: { sourceMap: true } },
+              { loader: 'sass-loader', options: { sourceMap: true } }]
+          })
+        }
 
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    plugins: [
+      CSSExtract
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
   }
 }
